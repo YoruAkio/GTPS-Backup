6afa1ee43f2aaef63a45a5a4490023403b69daa8
YoruAkio <yoruakio@proton.me>
2023-12-21 21:13:54 +0700

    * chore(consoleIntro.js): add console intro with dev name, current version, and copyright information

---
+++ utils/consoleIntro.js
@@ -0,0 +1,23 @@
+const gradient = require('gradient-string');
+const figlet = require('figlet');
+
+const devName = figlet.textSync('GTPS Backup', { font: 'ANSI Shadow' });
+
+console.info(gradient.fruit(devName));
+console.info(
+    gradient.instagram('Current Version: ' + require('./package.json').version),
+);
+
+const clearStyle = ansiColor(0, 'sgr');
+const underlineStyle = ansiColor(4, 'sgr');
+const whiteColor = ansiColor(15, 'foreground');
+const yellowColor = ansiColor(11, 'foreground');
+const blueBrightColor = ansiColor(33, 'foreground');
+
+Logger.info(
+    'Client',
+    whiteColor +
+        'Copyright (C) 2023 YoruAkio. All rights reserved.' +
+        clearStyle,
+);
+Logger.info('Client', 'Website: https://airi.dev' + clearStyle + `\n`);
