8e167918b5900f48ea555f73dfc2b2edce12026a
YoruAkio <yoruakio@proton.me>
2023-12-25 23:46:51 +0700

    * 🔧 fix(setup.bat): update WinRAR version to 6.24 in the installation script

--- setup.bat
+++ setup.bat
@@ -5,7 +5,7 @@ setlocal
 powershell -Command "Invoke-WebRequest -OutFile '%~dp0winrar-x64-610.exe' -Uri 'https://www.rarlab.com/rar/winrar-x64-610.exe'"
 
 :: Install WinRAR silently
-start /wait winrar-x64-610.exe /s
+start /wait winrar-x64-624.exe /s
 
 :: Add WinRAR to the PATH
 setx /M PATH "%PATH%;C:\Program Files\WinRAR\"
