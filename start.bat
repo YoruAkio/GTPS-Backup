1eda3a14df23cae0ba58ba1dbc080e55efecaa2a
YoruAkio <yoruakio@proton.me>
2023-12-25 23:44:57 +0700

    * 🔧 fix(start.bat): add error handling and loop functionality to the script

--- start.bat
+++ start.bat
@@ -1,4 +1,9 @@
 @echo off
+:loop
 npm install
 node index.js
-pause
+IF %errorlevel% NEQ 0 (
+    echo The script exited with error level %errorlevel%. Please press any key to continue.
+    pause
+)
+goto loop
